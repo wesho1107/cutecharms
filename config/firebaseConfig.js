a69b5ae143852062e7ee8b49293f8ab759473521
Timothy Putra Prasetio <timothy.prasetio@gmail.com>
2025-01-18 18:32:40 +0800

    Refactor Firebase configuration to remove Firestore initialization

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, getFirestore } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,6 +19,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app);
 
 export default app;
