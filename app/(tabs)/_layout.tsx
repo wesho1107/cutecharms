01af4463bbab7bcba5c14e7589ee6abfe110856e
Timothy Putra Prasetio <timothy.prasetio@gmail.com>
2025-01-18 15:23:22 +0800

    Add animation to tab layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -7,6 +7,7 @@ export default function TabLayout() {
       screenOptions={{
         tabBarActiveTintColor: 'darkblue',
         headerTitleAlign: 'center',
+        animation: 'shift',
       }}
     >
       <Tabs.Screen
